0bb897b6703ee81cb07c26a14cd8c81daa53b14d
doodles <nmedevelopercommunity@gmail.com>
2025-11-16 23:39:17 +0530

    first

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,25 @@
-const toggle = document.getElementById("menu-toggle");
-      const menu = document.getElementById("menu");
+ const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("menu");
+  const menuLinks = menu.querySelectorAll("a");
 
-      toggle.addEventListener("click", () => {
-        toggle.classList.toggle("active");
-        menu.classList.toggle("show");
-      }); 
+  // Toggle open/close when clicking hamburger
+  toggle.addEventListener("click", () => {
+    toggle.classList.toggle("active");
+    menu.classList.toggle("show");
+  });
+
+  // Close menu when clicking any link
+  menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      toggle.classList.remove("active");
+      menu.classList.remove("show");
+    });
+  });
+
+const titles = [ "Artist", "Freelancer"];
+  let index = 0;
+
+  setInterval(() => {
+    index = (index + 1) % titles.length;
+    document.getElementById("dynamic-text").textContent = titles[index];
+  }, 2000); // Change every 2 seconds
